58a1424b940a757baeda3fbef680708082369a5d
unknown <pschmidt@pasit.nl>
2024-10-28 11:18:07 +0100

    day1

---
+++ Live/Dag1/Basics/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Basics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Typenaam variabelenaam;
+        int age = 10;
+        Console.WriteLine(age);
+        int leeftijd = (age + 20) * 3;
+
+        //age = age + 1;
+        //age += 1;
+        //++age;
+        Console.WriteLine(++age);
+
+        bool ba = false;
+        bool bb = true;
+
+        bool bres = ba || bb;
+
+        bool bres2 = ba & bb;
+        int ia = 1;
+        int ib = 2;
+
+        int ires = ia | ib;
+        Console.WriteLine(ires);
+
+        bres = ba && Ok();
+        Console.WriteLine(bres);
+
+        char? test = null;
+        int x = test ?? 42;
+
+        string? s = null;
+
+        //if (s != null)
+            Console.WriteLine(s!.Length);
+    }
+
+    static bool Ok()
+    {
+        Console.WriteLine("In ok");
+        return false;
+    }
+}
---
+++ Live/Dag1/Flows/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Flows;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+
+        }
+        while (true);
+
+        while (true)
+        {
+            //Console.WriteLine("Iteratie");
+        }
+        //int idx = 0;
+        //for (Console.WriteLine("Init"); ;Console.WriteLine("Eind iteratie"))
+        for (int idx = 0; idx < 10; idx++)
+        {
+            //idx += 2;
+           // Console.WriteLine(idx);
+            //if (idx == 10) break;
+        }
+        //Console.WriteLine(idx);
+
+        int age = 10;
+        // if-else wordt met  name gebruikt voor range checks
+        if (age <= 10)
+        {
+
+        }
+        else if (age <= 20)
+        {
+
+        }
+        else
+        {
+
+        }
+
+        // Gebruik de switch voor concrete waarde tests.
+        switch (age)
+        {
+            case 0:
+            case 1:
+                Console.WriteLine("een");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case > 10:
+                break;
+            default:
+                break;
+        }
+    }
+}
